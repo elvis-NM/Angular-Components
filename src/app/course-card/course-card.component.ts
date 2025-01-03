65118595fba75a4710cf14d35f38ab900dec0a95
Elvis Makia <elvis_makia@yahoo.com>
2025-01-03 14:26:06 -0500

    pass input data in components

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'course-card',
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit{
+  
+  @Input()
+  title:string;
+  
+  ngOnInit(): void {
+    //
+  }
+
 
 }
